807965cd3b7614bad341f17a6a5d509012b34f1e
Nick Welch <mack@incise.org>
2005-02-17 23:37:26 -0800

    more scrunching
    darcs-hash:20050218073726-25792-688b0ba80797cb1fc9bd60efe34f1f7d5e682f66.gz

--- tinywm.c
+++ tinywm.c
@@ -1,11 +1,10 @@
-/* TinyWM (C) 2005 Nick Welch
- *
- * Usage of the works is permitted provided that this
- * instrument is retained with the works, so that any entity
- * that uses the works is notified of this instrument.
- *
- * DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
- */
+// TinyWM (C) 2005 Nick Welch
+//
+// Usage of the works is permitted provided that this
+// instrument is retained with the works, so that any entity
+// that uses the works is notified of this instrument.
+//
+// DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
 
 #include <X11/Xlib.h>
 
@@ -18,28 +17,29 @@ int main()
     if(!dpy) return 1;
 
     Window root = DefaultRootWindow(dpy);
-    KeyCode f1 = XKeysymToKeycode(dpy, XStringToKeysym("F1"));
 
-    XGrabKey(dpy, f1, Mod1Mask, root, True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask, root,
+            True, GrabModeAsync, GrabModeAsync);
     XGrabButton(dpy, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
             GrabModeAsync, None, None);
     XGrabButton(dpy, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
             GrabModeAsync, None, None);
 
-    XWindowAttributes initial;
-    XEvent ev;
+    XWindowAttributes attr;
     XButtonEvent start;
+    XEvent ev;
     for(;;)
     {
         XNextEvent(dpy, &ev);
-
-        if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
+        if(ev.type == KeyPress && ev.xkey.subwindow != None)
+            XRaiseWindow(dpy, ev.xkey.subwindow);
+        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
         {
-            start = ev.xbutton;
             XGrabPointer(dpy, ev.xbutton.subwindow, True,
                     PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
                     GrabModeAsync, None, None, CurrentTime);
-            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &initial);
+            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+            start = ev.xbutton;
         }
         else if(ev.type == MotionNotify)
         {
@@ -47,16 +47,13 @@ int main()
             int xdiff = ev.xbutton.x_root - start.x_root;
             int ydiff = ev.xbutton.y_root - start.y_root;
             XMoveResizeWindow(dpy, ev.xmotion.window,
-                    initial.x + (start.button == 1 ? xdiff : 0),
-                    initial.y + (start.button == 1 ? ydiff : 0),
-                    MAX(1, initial.width + (start.button == 3 ? xdiff : 0)),
-                    MAX(1, initial.height + (start.button == 3 ? ydiff : 0)));
+                attr.x + (start.button==1 ? xdiff : 0),
+                attr.y + (start.button==1 ? ydiff : 0),
+                MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
+                MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
         }
         else if(ev.type == ButtonRelease)
             XUngrabPointer(dpy, CurrentTime);
-        else if(ev.type == KeyPress && ev.xkey.subwindow != None &&
-                ev.xkey.keycode == f1 && ev.xkey.state == Mod1Mask)
-            XRaiseWindow(dpy, ev.xkey.subwindow);
     }
 }
 
