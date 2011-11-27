1108aa6e768fe0aa510dfef5b4573146bd827211
Aleksi Torhamo <aleksi@torhamo.net>
2011-11-27 16:35:32 +0200

    Fixed clicking on the root window
    The code wasn't checking if we've actually started dragging.
    If you started dragging on the root window before dragging
    any real window, the code would crash.
    If you started dragging on the root window after dragging
    some real window, the window you dragged last would jump
    to the cursor.

--- tinywm.py
+++ tinywm.py
@@ -10,11 +10,12 @@ dpy = Display()
 
 dpy.screen().root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("F1")), X.Mod1Mask, 1,
         X.GrabModeAsync, X.GrabModeAsync)
-dpy.screen().root.grab_button(1, X.Mod1Mask, 1, X.ButtonPressMask|X.PointerMotionMask,
+dpy.screen().root.grab_button(1, X.Mod1Mask, 1, X.ButtonPressMask|X.ButtonReleaseMask|X.PointerMotionMask,
         X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE)
-dpy.screen().root.grab_button(3, X.Mod1Mask, 1, X.ButtonPressMask|X.PointerMotionMask,
+dpy.screen().root.grab_button(3, X.Mod1Mask, 1, X.ButtonPressMask|X.ButtonReleaseMask|X.PointerMotionMask,
         X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE)
 
+start = None
 while 1:
     ev = dpy.next_event()
     if ev.type == X.KeyPress and ev.child != X.NONE:
@@ -23,7 +24,7 @@ while 1:
         print 'press'
         attr = ev.child.get_geometry()
         start = ev
-    elif ev.type == X.MotionNotify:
+    elif ev.type == X.MotionNotify and start:
         print 'motion'
         xdiff = ev.root_x - start.root_x
         ydiff = ev.root_y - start.root_y
@@ -32,4 +33,6 @@ while 1:
             y = attr.y + (start.detail == 1 and ydiff or 0),
             width = max(1, attr.width + (start.detail == 3 and xdiff or 0)),
             height = max(1, attr.height + (start.detail == 3 and ydiff or 0)))
+    elif ev.type == X.ButtonRelease:
+        start = None
 
